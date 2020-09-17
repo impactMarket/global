4ede65cca7dd9a13581ffe8653a9ec691d42911e
Bernardo Vieira <bernardo5@live.com.pt>
2020-09-17 17:07:42 +0100

    homepage + fix global values

--- src/components/GlobalScanner.tsx
+++ src/components/GlobalScanner.tsx
@@ -27,20 +27,35 @@ export default function GlobalScanner() {
         {
             title: 'Total Raised',
             value: '2,360,401',
+            isMoney: true,
         },
         {
             title: 'Total Distributed',
             value: '952,778',
+            isMoney: true,
         },
         {
             title: 'Total Beneficiaries',
             value: '6,912',
+            isMoney: false,
         },
         {
             title: 'Total Claims',
             value: '380,847',
+            isMoney: false,
         },
     ];
+
+    const dataText = (data: { title: string, value: string, isMoney: boolean }) => {
+        if (data.isMoney) {
+            return <Typography variant="body1" className={classes.description} gutterBottom>
+                <span style={{ fontFamily: 'Gelion', fontWeight: 550, fontSize: 30 }}>${data.value}</span> cUSD
+            </Typography>;
+        }
+        return <Typography variant="body1" className={classes.description} gutterBottom>
+            <span style={{ fontFamily: 'Gelion', fontWeight: 550, fontSize: 30 }}>{data.value}</span>
+            </Typography>;
+    }
 
     return <Grid container className={classes.root} spacing={2}>
         <Grid item xs={12}>
@@ -51,9 +66,7 @@ export default function GlobalScanner() {
                             <Typography variant="h6" className={classes.title} gutterBottom>
                                 {total.title}
                             </Typography>
-                            <Typography variant="body1" className={classes.description} gutterBottom>
-                                <span style={{ fontFamily: 'Gelion', fontWeight: 550, fontSize: 30 }}>${total.value}</span> cUSD
-                        </Typography>
+                            {dataText(total)}
                         </Paper>
                     </Grid>
                 ))}
